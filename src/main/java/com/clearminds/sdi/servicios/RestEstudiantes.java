d255f99839c3f99ea1a8211621de5e85f1a227d0
Sofía Díaz <soufito21@gmail.com>
2020-12-09 15:10:35 -0500

    Ingreso y actualización de datos con WebServices

--- src/main/java/com/clearminds/sdi/servicios/RestEstudiantes.java
+++ src/main/java/com/clearminds/sdi/servicios/RestEstudiantes.java
@@ -3,7 +3,9 @@ package com.clearminds.sdi.servicios;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+import com.clearminds.sdi.bdd.ConexionBDD;
 import com.clearminds.sdi.dtos.Estudiante;
+import com.clearminds.sdi.excepciones.BDDException;
 
 @Path("/estudiantes")
 public class RestEstudiantes {
@@ -12,6 +14,34 @@ public class RestEstudiantes {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void insertar(Estudiante est){
+		ServicioEstudiante s=new ServicioEstudiante();
+		try {
+			s.insertarEstudiante(est);
+		} catch (BDDException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		System.out.println("Ingresando persona: "+est);
+	}
+	
+	
+	@Path("/actualizar")
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void actualizar(Estudiante est){
+		ServicioEstudiante s=new ServicioEstudiante();
+		try {
+			if(est!=null){
+				s.actualizarEstudiante(est);
+			}
+			s.actualizarEstudiante(est);
+		} catch (BDDException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("Actualizando estudiante: "+est);
 	}
 }
